17d6c2de48ce056772b92caa6fdf3f76607767a1
Rachel Dotey <rachel.dotey@gmail.com>
2023-04-19 13:14:06 -0400

    Update AppFooter.tsx

--- components/AppFooter.tsx
+++ components/AppFooter.tsx
@@ -1,14 +1,25 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 const copyright = `© 2012-${new Date().getFullYear()} Rachel Dotey, Web & App Development`;
 
 export default function AppFooter() {
   return (
     <footer>
-      <Container maxWidth="lg" sx={{ marginTop: 8 }}>
-        <p>{copyright}</p>
-      </Container>
+      <Box
+        sx={{
+          marginTop: 8,
+          paddingY: 4,
+          borderTop: 1,
+          borderColor: 'grey.400',
+        }}
+      >
+        <Container maxWidth="lg" sx={{ color: 'text.secondary' }}>
+          <Typography variant="body2">{copyright}</Typography>
+        </Container>
+      </Box>
     </footer>
   );
 }
