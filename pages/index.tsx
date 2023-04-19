fa08806b9931a70119598ab44dfe94c8b1cec1f7
Rachel Dotey <rachel.dotey@gmail.com>
2023-04-19 15:36:15 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,57 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
+
+import styles from '@/styles/Home.module.css';
+import iDesignExperiencesPic from '../assets/images/laptop-i-design-experiences.jpg';
+
+const inter = Inter({ subsets: ['latin'] });
+
+const AboveFoldSection: React.FC = () => {
+  return (
+    <Box sx={{ typography: 'body1' }}>
+      <Container maxWidth="lg" sx={{ color: 'text.primary' }}>
+        <Typography variant="body1">Hi, my name is Rachel!</Typography>
+        <Typography variant="body1">I build responsive web apps.</Typography>
+      </Container>
+    </Box>
+  );
+};
+
+const WelcomeSection: React.FC = () => {
+  return (
+    <Container maxWidth="lg" sx={{ color: 'text.primary' }}>
+      <Grid container spacing={1}>
+        <Grid xs={4} display="flex" justifyContent="center" alignItems="center">
+          <Box>
+            <Image
+              src={iDesignExperiencesPic}
+              alt="Picture of the author"
+              height={260}
+              width={260}
+            />
+          </Box>
+        </Grid>
+        <Grid xs={8} display="flex" alignItems="center">
+          <Box>
+            <Typography variant="body1">Hello and welcome!</Typography>
+            <Typography variant="body1">
+              I'm a software developer with a passion for creating innovative
+              solutions using the latest technologies. Whether you need a
+              responsive web design, a robust application, or a cutting-edge
+              system integration, I am here to help.
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
 export default function Home() {
   return (
@@ -15,100 +63,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <AboveFoldSection />
+        <WelcomeSection />
       </main>
     </>
-  )
+  );
 }
