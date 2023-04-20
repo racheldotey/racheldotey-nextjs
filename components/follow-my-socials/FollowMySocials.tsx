ae05f88ad4ee328fa753cb353ed9d146c745361e
Rachel Dotey <rachel.dotey@gmail.com>
2023-04-19 20:57:56 -0400

    Socials styles

--- components/follow-my-socials/FollowMySocials.tsx
+++ components/follow-my-socials/FollowMySocials.tsx
@@ -3,9 +3,29 @@ import Image from 'next/image';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 
+import socialsCss from './socials.module.css';
+
 import githubIcon from './icons/github.png';
 import linkedinIcon from './icons/linkedin.png';
 import twitterIcon from './icons/twitter.png';
+
+const SocialIcon: React.FC<{ alt: string; href: string; src: string }> = ({
+  alt,
+  href,
+  src,
+}) => {
+  return (
+    <a href={href} title={alt} target="_blank" rel="noopener noreferrer">
+      <Image
+        src={src}
+        alt={alt}
+        height={32}
+        width={32}
+        className={socialsCss.bounceAnimation}
+      />
+    </a>
+  );
+};
 
 export default function FollowMySocials() {
   return (
@@ -16,9 +36,21 @@ export default function FollowMySocials() {
         alignItems="center"
         spacing={1}
       >
-        <Image src={githubIcon} alt="GitHub" height={32} width={32} />
-        <Image src={linkedinIcon} alt="GitHub" height={32} width={32} />
-        <Image src={twitterIcon} alt="GitHub" height={32} width={32} />
+        <SocialIcon
+          alt="GitHub"
+          href="https://github.com/racheldotey"
+          src={githubIcon}
+        />
+        <SocialIcon
+          alt="GitHub"
+          href="https://www.linkedin.com/in/racheldotey"
+          src={linkedinIcon}
+        />
+        <SocialIcon
+          alt="GitHub"
+          href="https://twitter.com/RaeDeveloper"
+          src={twitterIcon}
+        />
       </Stack>
     </Box>
   );
