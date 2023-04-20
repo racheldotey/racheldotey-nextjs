68016c13de94d0756302167ee9d6231f260b69b7
Rachel Dotey <rachel.dotey@gmail.com>
2023-04-20 13:16:42 -0400

    Update _document.tsx
    Warning to all who come after

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+/**
+ * This file is only rendered on the server,
+ * so event handlers like onClick cannot be used in _document.
+ * @see https://nextjs.org/docs/advanced-features/custom-document
+ */
 export default function Document() {
   return (
     <Html lang="en">
